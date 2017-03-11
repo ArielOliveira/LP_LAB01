a6f01c6ac54ea7e9fb92c40d0fee7c25f5096343
Gabriel Barbosa <gbsbarbosa.gb@gmail.com>
2017-03-11 02:49:48 -0300

    correção: arquivos de cabeçalho e corpo adicionados. calcvolume funcionando para todas as opções

---
+++ include/volume.h
@@ -0,0 +1,10 @@
+#ifndef VOLUME_H
+#define VOLUME_H
+
+float volume_piramide(float &base, float &altura);
+float volume_cubo(float &aresta);
+float volume_paralelepipedo(float &aresta1, float &aresta2, float &aresta3);
+float volume_esfera(float &raio);
+
+
+#endif
